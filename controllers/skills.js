66f917234f809201e2914c609d934eb691a6d859
Robin <daorob@gmail.com>
2019-08-22 02:16:15 -0500

    Update show function. Create newSkill/create/deleteSkill functions.

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,13 +2,31 @@ var Skill = require('../module/skill');
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill
+};
+
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
+    res.redirect('/');
+};
+
+function create(req, res) {
+    Skill.addOne(req.body);
+    res.redirect('/');
+};
+
+function newSkill(req, res) {
+    res.render('skills/new');
 };
 
 function show(req, res) {
     res.render('skills/show', {
         skills: Skill.getOne(req.params.id),
-        skillNum: parseInt(req.params.id) + 1
+        skillNum: parseInt(req.params.id) + 1,
+        idx: req.params.id
     });
 };
 
